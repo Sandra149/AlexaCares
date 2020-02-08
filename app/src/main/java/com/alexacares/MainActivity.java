5d245287e28c37c4770d747b7046ac0ee895350f
Sandra <sandratorres149@gmail.com>
2020-02-09 00:16:50 +0100

    Added Toolbar
    Hemos hecho un huevo de cosas

--- app/src/main/java/com/alexacares/MainActivity.java
+++ app/src/main/java/com/alexacares/MainActivity.java
@@ -1,6 +1,7 @@
 package com.alexacares;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 
@@ -10,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initToolbar();
+    }
+
+    /**
+     * Quita la Toolbar de Android por defecto y el mensaje
+     */
+    private void initToolbar() {
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+        }
     }
 }
